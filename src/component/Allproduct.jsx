16e500e1a916217aa99c833970a6e6502a133591
Md. Abdul Alim <32503438+alim84@users.noreply.github.com>
2025-02-23 13:12:51 +0600

    Pagination

--- src/component/Allproduct.jsx
+++ src/component/Allproduct.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import Product from './Product';
+import Pagination from './Pagination';
 
 const Allproduct = () => {
     return (
         <aside>
             <div className='grid grid-cols-3 gap-3'>
+                {/* <Product/>
                 <Product/>
                 <Product/>
                 <Product/>
                 <Product/>
                 <Product/>
-                <Product/>
-                <Product/>
+                <Product/> */}
+                <Pagination itemsPerPage={6}/>
             </div>
         </aside>
     );
